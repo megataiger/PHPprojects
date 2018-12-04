51149d8b797e93b17df724516f1b6a4a3e01db98
megataiger <45592104+megataiger@users.noreply.github.com>
2018-12-04 15:20:30 +0400

    Add files via upload

--- Table.php
+++ Table.php
@@ -1,6 +1,27 @@
+ <body>
+ <table border="1">
  <?php
-    $n="Hi, student!";
-    echo $n;
- 
- 
+    $n_max = 10;
+    $m_max = 10;
+    $n_min = 0;
+    $m_min = 0;
+    for ($i = $n_min; $i <= $n_max; $i++)
+    {
+        print "\t<tr>\n";
+        for ($j = $m_min; $j <= $m_max; $j++)
+        {
+            $p = $i*$j;
+            if (($i==0)&&($j==0)){
+            print "\t\t<td></td>\n";}
+            else if ($j==0){
+            print "\t\t<td><b>$i</b></td>\n";}
+            else if ($i==0) {
+            print "\t\t<td><b>$j</b></td>\n";}
+            else
+            {print "\t\t<td>$p</td>\n";}
+        }
+        print "\t</tr>\n";
+    }
  ?>
+ </table>
+ </body>
