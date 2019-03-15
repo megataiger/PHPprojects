45041ef7df17125b256162b1244bdc492ac90999
megataiger <45592104+megataiger@users.noreply.github.com>
2019-03-15 13:27:56 +0000

    Update model.php
    sdjfposdj

--- model.php
+++ model.php
@@ -1,6 +1,6 @@
  <?php
+$link = mysqli_connect("127.0.0.1","student","student","student");
 
- $menu = 'Hello vasya';
- $body = 'So, hello'
- 
+$menu = print_r(mysqli_fetch_array(mysqli_query ($link, 'select * from 321_Nemolyaev_CMS ')), 1);
+ $body = 'So, hello';
  ?>
